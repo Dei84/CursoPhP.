f9e0b959bad2254641c3add519a4e2a129bff8f4
deivintir <d4vidyeva@gmail.com>
2022-03-07 12:38:52 +0100

    Añadidos comentarios al ejercicio.

--- 11ª_clase.php
+++ 11ª_clase.php
@@ -10,12 +10,19 @@
     <?php 
     //##php18.
         $var1 = 1;
-
+        // El ciclo while accede al código del interior del bucle solo si la condición se cumple.
         while ($var1 <= 6) {
             echo "Estamos ejecutando el bucle en su iteración nº $var1.</br>";
             $var1++;
         }
-        echo "Hemos salido del ciclo while."
+        echo "Hemos salido del ciclo while.";
+        //El ciclo do-While ejecuta el codigo del interior al menos una vez, ya que el flujo de ejecución permite ejecutar el código antes de la comprobación de la condición.
+        do {
+            echo "Estamos ejecutando un bucle do-while en su iteración nº $var1.</br>";
+            $var1++;
+        } while ($var1 <= 6);
+        echo "Ha terminado el ciclo do-while.";
     ?>
+
 </body>
 </html>
