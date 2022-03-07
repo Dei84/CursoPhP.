6f602b3be819c99435a1c57279dfe667b8e65cbe
deivintir <d4vidyeva@gmail.com>
2022-03-07 12:29:06 +0100

    Ciclo while

---
+++ 11ª_clase.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    //##php18.
+        $var1 = 1;
+
+        while ($var1 <= 6) {
+            echo "Estamos ejecutando el bucle en su iteración nº $var1.</br>";
+            $var1++;
+        }
+        echo "Hemos salido del ciclo while."
+    ?>
+</body>
+</html>
